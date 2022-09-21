f4f552643da8864625f085fe494fbe2ad74b74dc
Silvia Saverino <silviasave89@gmail.com>
2022-09-21 22:23:21 +0000

    add function to check collision

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const player = document.getElementById('dd-player');
 const obstacle = document.getElementById('dd-obstacle');
 
+/**makes the character jump*/
 function jump(){
     if(player.classList != 'up'){
         player.classList.add('up');
@@ -9,3 +10,22 @@ function jump(){
         player.classList.remove('up');
     },500);
 }
+
+/** check for collision every 15mSec*/
+
+var checkCollision = setInterval(function() {
+   var player= parseInt(window.getComputedStyle(player).getPropertyValue('top'));
+
+}, 10);
+
+var checkCollision = setInterval(function() {
+    var obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left'));
+
+    if(obstacleLeft < 25 && obstacle.Left > 10 && player >= 130){
+        obstacle.style.animation = 'none';
+        obstacle.style.display = 'none';
+        alert('collision');
+    }
+ 
+ }, 10);
+
