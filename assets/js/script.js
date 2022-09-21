b7e4139cb8a9c50624082ad093fc4ccbf436f89a
Silvia Saverino <silviasave89@gmail.com>
2022-09-21 20:24:55 +0000

    add function to let the character jump

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log('it works')
+const player = document.getElementById('dd-player');
+const obstacle = document.getElementById('dd-obstacle');
+
+function jump(){
+    if(player.classList != 'up'){
+        player.classList.add('up');
+    }
+    setTimeout(function(){
+        player.classList.remove('up');
+    },500);
+}
