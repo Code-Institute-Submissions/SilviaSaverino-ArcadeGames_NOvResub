fc1cc5fe1660bb96d328bdfb4cad5adaf64240b2
Silvia Saverino <silviasave89@gmail.com>
2022-09-24 14:18:02 +0000

    fix collision in game1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const player = document.getElementById('dd-player');
 const obstacle = document.getElementById('dd-obstacle');
-const playerHeight = 50;
-const playerWidth = 30;
+/*const playerHeight = 50;
+const playerWidth = 30;*/
 
 /**makes the character jump*/
 function jump(){
@@ -16,11 +16,11 @@ function jump(){
 /** check for collision every 15mSec*/
 
 var checkCollision = setInterval(function() {
-    var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
+    // var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
 
     var obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left'));
 
-    if(obstacleLeft < 20 && obstacle.Left > 0 && player >= 130){
+    if(obstacleLeft < 60 && obstacleLeft > 30 && !player.classList.contains('up')){
         obstacle.style.animation = 'none';
         obstacle.style.display = 'none';
         alert('collision');
