29141bed9b22be4b300ea987401cb7f96801e431
Silvia Saverino <silviasave89@gmail.com>
2022-09-27 20:22:28 +0000

    fixing code syntax for responsiviness and validators

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
+const goatBtn    = document.getElementById('goatBtn');
+const rockBtn    = document.getElementById('rockBtn');
+const fallingBtn = document.getElementById('fallingBtn');
 
-
-
-
-/* --------------------------------------------second-game: rock-paper-scissors -------------------------------------*/
-
-
-/*-----------------------------------------------third game-----------------------------------------------*/
+goatBtn.addEventListener('click', () => location.href="1stGame.html");
+rockBtn.addEventListener('click', () => location.href="2ndGame.html");
+fallingBtn.addEventListener('click', () => location.href="3rdGame.html");
